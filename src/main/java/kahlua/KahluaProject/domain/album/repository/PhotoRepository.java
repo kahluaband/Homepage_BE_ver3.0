fed095540655ca16f72efc990792e48f8bf4363b
Seungwon326 <sprjk0326@naver.com>
2026-05-19 14:52:32 +0900

    fix: 사진 다운로드 기능 해결

--- src/main/java/kahlua/KahluaProject/domain/album/repository/PhotoRepository.java
+++ src/main/java/kahlua/KahluaProject/domain/album/repository/PhotoRepository.java
@@ -7,8 +7,7 @@ import java.util.Optional;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long>, PhotoRepositoryCustom {
 
-    // 앨범 ID와 사진 ID가 일치하며 삭제되지 않은 사진 조회
-    Optional<Photo> findByIdAndAlbumIdAndDeletedAtIsNull(Long id, Long albumId);
+    Optional<Photo> findByIdAndAlbumId(Long id, Long albumId);
 
 
 }
